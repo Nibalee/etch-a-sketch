d09100d1dcc99273e29bae55364c2b789b06e11b
Nejd Benali <benalinejd12@gmail.com>
2023-03-04 13:35:53 +0800

    Coloring the  grid

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-const container = document.querySelector('.container');
-let size = 5;
-setupGrid(5);
-function setupGrid(size){
+let currentColor = '#333333'; 
+let size = 5;  
 
+const container = document.querySelector('.container');
+
+//setupGrid(size of grid)
+setupGrid(5);
+
+function setupGrid(size){
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for( var i=0; i<size;i++){
         for(var j=0; j<size;j++){
             let gridItem = document.createElement("div");
             gridItem.classList.add("grid-item");
+            gridItem.addEventListener('mouseover', changeColor);
+            gridItem.addEventListener('mousedown', changeColor);
             container.appendChild(gridItem);
         }
     }
+}
+
+//track of mouse click and mouse down
+let mouseDown = false;
+document.body.onmousedown = () =>(mouseDown=true);
+document.body.onmouseup = () => (mouseDown = false);
+
+//simple text no color mode specified
+function changeColor(e){
+    if(e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = currentColor;
 }
